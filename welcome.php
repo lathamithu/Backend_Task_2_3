1164b541f45dd8f0af0bb00f86c9c2ca2d21880f
Meghana <lathamithu2000@gmail.com>
2020-06-19 22:54:05 +0530

    Php to get and display

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+
+<label>First name :</label>
+<?php echo $_POST["fname"]; ?><br>
+<label>Last name :</label>
+<?php echo $_POST["lname"]; ?><br>
+<label>Date of birth :</label>
+<?php echo $_POST["date"]; ?><br>
+<label>Gender :</label>
+<?php echo $_POST["gender"]; ?><br>
+<label>Qualification :</label>
+<?php echo $_POST["education"]; ?><br>
+<label>Address :</label>
+<?php echo $_POST["address"]; ?><br>
+<br>	
+
+</body>
+</html>
